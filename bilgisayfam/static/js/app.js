7c8accc91449b88c1be3ee7add2aae9cc40411c5
Tayfun Sen <totayfun@gmail.com>
2013-05-27 17:27:03 +0300

    Integrate django pipeline.

--- bilgisayfam/static/js/app.js
+++ bilgisayfam/static/js/app.js
@@ -3,10 +3,7 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('bilgisayfam', ['bilgisayfam.filters', 'bilgisayfam.services', 'bilgisayfam.directives', 'bilgisayfam.controllers'])
-  .config(function($locationProvider){
+  .config(['$locationProvider', function($locationProvider){
     $locationProvider.html5Mode(true).hashPrefix("!");
-  }
+  }]
 );
-
-// Also define our own namespace.
-var bilgisayfam = {};
