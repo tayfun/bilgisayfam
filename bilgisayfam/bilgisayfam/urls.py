d58edaa1c67d05b1ce7a006aedaf27d6a9a9263e
Tayfun Sen <totayfun@gmail.com>
2013-05-29 17:12:09 +0300

    Cache manifest file for 15 mins.

--- bilgisayfam/bilgisayfam/urls.py
+++ bilgisayfam/bilgisayfam/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
+from django.views.decorators.cache import cache_page
+
 from manifesto.views import ManifestView
 
 
 urlpatterns = patterns('',
     url(r'^$', "entry.views.index_view"),
-    url(r'^manifest\.appcache$', ManifestView.as_view(),
+    url(r'^manifest\.appcache$', cache_page(60 * 15)(ManifestView.as_view()),
         name="cache_manifest"),
 )
